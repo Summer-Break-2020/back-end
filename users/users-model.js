bfd711ce567fd073378894ec42488699b8055631
unknown <dethgrenade@gmail.com>
2020-06-30 14:44:40 -0700

    GET /api/users/:id endpoint working

--- users/users-model.js
+++ users/users-model.js
@@ -9,7 +9,8 @@ module.exports = {
 
 async function add(user) {
     try {
-        const [id] = await db('users').insert(user, 'id')
+        const [id] = await db('users')
+            .insert(user, 'id')
 
         return findById(id)
     }
@@ -24,9 +25,12 @@ function find() {
 }
 
 function findBy(filter) {
-    return db('users').where(filter)
+    return db('users')
+        .where(filter)
 }
 
 function findById(id) {
-    return db('users').where({ id })
+    return db('users')
+        .where({ id })
+        .select('id', 'username', 'email', 'created_at', 'updated_at')
 }
--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,7 @@ const Users = require('./users-model')
 
 const requireAuth = require('../auth/requires-auth')
 
+//Returns a list of all users
 router.get('/', requireAuth, (req, res) => {
     Users.find()
         .then(users => {
@@ -12,6 +13,23 @@ router.get('/', requireAuth, (req, res) => {
             }
             else {
                 res.status(404).json({ message: 'no users found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    Users.findById(id)
+        .then(([user]) => {
+            if (user) {
+                res.status(200).json(user)
+            }
+            else {
+                res.status(404).json({ message: 'invalid user id' })
             }
         })
         .catch(error => {
