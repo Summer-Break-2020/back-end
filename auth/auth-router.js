df2b22991bae3efd53161d8e25f196ebbf67326f
unknown <dethgrenade@gmail.com>
2020-06-30 11:27:17 -0700

    added init db migration. register user endpoint working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,8 +6,15 @@ const { validateRegister } = require('../middleware/validation')
 //Users Model
 const Users = require('../users/users-model')
 
+//Helper FNs
+const { hashPassword } = require('../users/users-service')
+
+//Register a new user
 router.post('/register', validateRegister, (req, res) => {
     const userInfo = req.body
+
+    //Hash the password before adding user to DB
+    userInfo.password = hashPassword(userInfo.password)
 
     Users.add(userInfo)
         .then(user => {
