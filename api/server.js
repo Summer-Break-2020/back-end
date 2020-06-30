bc2bec80d6dbb4410fec2976857025232b510cd6
unknown <dethgrenade@gmail.com>
2020-06-30 10:23:21 -0700

    initial commit. Server framework created

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const server = express()
+
+//Middleware
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+//Endpoints
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up and running!' })
+})
+
+module.exports = server
